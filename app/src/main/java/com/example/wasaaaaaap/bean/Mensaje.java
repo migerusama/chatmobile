bc339fa1b7ee54428bcff4b6f37f57e16ef38282
migerusama <migueldepablos99@gmail.com>
2022-02-23 00:50:51 +0100

    init

---
+++ app/src/main/java/com/example/wasaaaaaap/bean/Mensaje.java
@@ -0,0 +1,36 @@
+package com.example.wasaaaaaap.bean;
+
+public class Mensaje {
+    public static final int TYPE_RECEIVED = 0;// Indica que este es un mensaje recibido
+    public static final int TYPE_SENT = 1;// Indica que este es un mensaje enviado
+    private String content;
+    private int type;
+
+    /**
+     * Indica el contenido del mensaje y tipo indica el tipo de mensaje. Los dos valores anteriores son opcionales para el tipo de mensaje.
+     */
+    public Mensaje(String content, int type) {
+        this.content = content;
+        this.type = type;
+    }
+
+    public Mensaje(String content) {
+        this.content = content;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ app/src/main/java/com/example/wasaaaaaap/server/Servidor.java
@@ -0,0 +1,140 @@
+package com.example.wasaaaaaap.server;
+
+import com.example.wasaaaaaap.bean.Mensaje;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Servidor implements Runnable {
+
+    private ArrayList<ConnectionHandler> connections;
+    private ServerSocket server;
+    private boolean done;
+    private ExecutorService pool;
+
+    public Servidor() {
+        connections = new ArrayList<>();
+        done = false;
+    }
+
+    @Override
+    public void run() {
+        try {
+            server = new ServerSocket(9999);
+            pool = Executors.newCachedThreadPool();
+            while (!done) {
+                Socket client = server.accept();
+                ConnectionHandler handler = new ConnectionHandler(client);
+                connections.add(handler);
+                pool.execute(handler);
+            }
+        } catch (Exception e) {
+            shutdown();
+        }
+    }
+
+    public void broadcast(Mensaje message) {
+        for (ConnectionHandler ch : connections) {
+            if (ch != null) ch.sendMessage(message);
+        }
+    }
+
+    public void shutdown() {
+        try {
+            done = true;
+            pool.shutdown();
+            if (!server.isClosed()) {
+                server.close();
+            }
+            for (ConnectionHandler ch : connections) {
+                ch.shutdown();
+            }
+        } catch (Exception e) {
+            // ignore
+        }
+    }
+
+    class ConnectionHandler implements Runnable {
+
+        private Socket client;
+        private ObjectInputStream in;
+        private ObjectOutputStream out;
+
+        public ConnectionHandler(Socket client) {
+            this.client = client;
+        }
+
+        @Override
+        public void run() {
+            try {
+                out = new ObjectOutputStream(client.getOutputStream());
+                in = new ObjectInputStream(client.getInputStream());
+                Mensaje message;
+                while ((message = (Mensaje) in.readObject()) != null) {
+                    broadcast(message);
+                }
+            } catch (Exception e) {
+                shutdown();
+            }
+        }
+
+        public void sendMessage(Mensaje message) {
+            try {
+                out.writeObject(message);
+            } catch (IOException e) {
+                shutdown();
+            }
+        }
+
+        public void shutdown() {
+            try {
+                in.close();
+                out.close();
+                if (!client.isClosed()) client.close();
+            } catch (IOException e) {
+                // ignore
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Servidor servidor = new Servidor();
+        servidor.run();
+    }
+
+
+
+
+
+
+
+
+
+
+
+    /*public static void main(String[] args) throws Exception {
+        byte[] bytes = new byte[1024];
+        DatagramSocket socket = new DatagramSocket(8888);
+        DatagramPacket dp = new DatagramPacket(bytes, bytes.length);
+        socket.receive(dp);
+        String texto = new String(dp.getData());
+        InetAddress ipOrigen = dp.getAddress();
+        int puerto = dp.getPort();
+        byte[] mensaje = new byte[1024];
+        String Saludo="Devuelvo Saludos !!";
+        mensaje = Saludo.getBytes();
+        DatagramPacket envio = new DatagramPacket(mensaje, mensaje.length, ipOrigen, puerto);
+        socket.send(envio);
+        socket.close();
+
+    }*/
+}
